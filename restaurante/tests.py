c50c612070407bef6d200568c13b1e88ccd6fdeb
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-16 13:20:41 +0100

    Ansible, Vagrant y Travis

--- restaurante/tests.py
+++ restaurante/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+from restaurante.views import *
+from restaurante.models import Bar
+from django.test.client import RequestFactory
+# Create your tests here.Anadido testeo rutas
+
+
+
+class BarMethodTests(TestCase):
+
+	def test_nombre_bar(self):
+		bar = Bar(nombre='BarPaco' ,direccion='Recogidas', numerovisitas='500')
+		per.save()
+		self.assertEqual(bar.nombre,'BarPaco')
+		self.assertEqual(bar.direccion,'Recogidas')
+		self.assertEqual(bar.numerovisitas,'500')
+		print("Testeo correcto.")
