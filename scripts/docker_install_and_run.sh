3635082f888b63ccbf071f89b1e81a1e3df34835
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-17 21:48:31 +0100

    docker

--- scripts/docker_install_and_run.sh
+++ scripts/docker_install_and_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y docker.io
-sudo docker pull javiergarridomellado/iv_javiergarridomellado:apuestas
-sudo docker run -t -i javiergarridomellado/iv_javiergarridomellado:apuestas /bin/bash
+sudo docker pull javiergarridomellado/dai:dai
+sudo docker run -t -i javiergarridomellado/dai:dai /bin/bash
