21619a96444840d8b3d465a19a5b4e479a46f89c
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-19 08:51:50 +0100

    tests

--- restaurante/tests.py
+++ restaurante/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 
 from restaurante.views import *
-from restaurante.models import Bar
+from restaurante.models import Bar,Tapa
 from django.test.client import RequestFactory
 # Create your tests here.Anadido testeo rutas
 
@@ -18,4 +18,15 @@ class BarMethodTests(TestCase):
 		self.assertEqual(bar.nombre,'BarPaco')
 		self.assertEqual(bar.direccion,'Recogidas')
 		self.assertEqual(bar.numerovisitas,'500')
+		print("Testeo correcto.")
+
+	def setUp(self):
+		Bar.objects.create(nombre="BarVietto", direccion="Granada",numerovisitas='500')
+		Bar.objects.create(nombre="BarPaco", direccion="Albolote",numerovisitas='500')
+
+	def test_bar_ciudad(self):
+		bVietto = Bar.objects.get(nombre="BarVietto")
+		bPaco = Bar.objects.get(nombre="BarPaco")
+		self.assertEqual(bVietto.direccion,'Granada')
+		self.assertEqual(bPaco.direccion,'Albolote')
 		print("Testeo correcto.")
