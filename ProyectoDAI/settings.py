cdfe61dd4cdd14228fe0ca9b09f7595dfaae30f3
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-20 20:02:55 +0100

    respaldo

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -83,12 +83,23 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vagrant',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 #ON_HEROKU = os.environ.get('PORT')
 #if ON_HEROKU:
 #	DATABASE_URL='postgres://kytzveedsclzaf:eIJAAuElYvSxPK-vmSdXG9Hjv8@ec2-107-21-219-235.compute-1.amazonaws.com:5432/df9sfr7a9b8vjf'
