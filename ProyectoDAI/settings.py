37dac08648ff9029d1f441c890c508a40916b274
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-05 18:33:34 +0100

    settings

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -85,22 +85,22 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'vagrant',
-#        'USER': 'root',
-#        'PASSWORD':'',
-#        'HOST': '',
-#        'PORT': '',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vagrant',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 
 ON_HEROKU = os.environ.get('PORT')
