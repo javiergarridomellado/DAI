c3d93416de2dc030a6792c9698b976ce2397e66a
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-05 11:43:54 +0100

    script de despliegue en Azure

--- scripts/deploy_Azure.sh
+++ scripts/deploy_Azure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/javiergarridomellado/DAI.git
+git clone https://github.com/javiegarridomellado/DAI.git
 cd DAI/VagrantIV/
 chmod 777 create_and_run.sh
 ./create_and_run.sh
