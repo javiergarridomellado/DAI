be9494c71ce2360fc3981f4d094aa3061e94e3ea
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-08 16:31:48 +0100

    Arranque app en modo desarrollo

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,13 @@ from fabric.api import run, local, hosts, cd
 from fabric.contrib import django
 
 
-
+#Descarga y arranca docker
 def install_run():
 	run('sudo apt-get update')
 	run('sudo apt-get install -y docker.io')
 	run('sudo docker pull javiergarridomellado/dai:dai')
 	run('sudo docker run -i -t javiergarridomellado/dai:dai /bin/bash')
+
+#Ejecucion de la aplicacion en modo desarrollo
+def runApp():
+	run('sudo python DAI/manage.py runserver 0.0.0.0:80')
