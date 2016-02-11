8e3871117deba8c627679df2066e6928869c82af
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-11 23:01:30 +0100

    Fabfile modificado

--- fabfile.py
+++ fabfile.py
@@ -2,13 +2,6 @@ from fabric.api import run, local, hosts, cd
 from fabric.contrib import django
 
 
-#Descarga y arranca docker
-def install_run():
-	run('sudo apt-get update')
-	run('sudo apt-get install -y docker.io')
-	run('sudo docker pull javiergarridomellado/dai:dai')
-	run('sudo docker run -i -t javiergarridomellado/dai:dai /bin/bash')
-
 #Ejecucion de la aplicacion en modo desarrollo
 def runApp():
 	run('cd DAI && sudo python manage.py runserver 0.0.0.0:80')
@@ -16,3 +9,34 @@ def runApp():
 #Actualizar la aplicacion
 def actApp():
 	run('cd DAI && sudo git pull')
+
+#Realizar testeo de la aplicacion
+def testApp():
+	run('cd DAI && sudo python manage.py test')
+
+#Parar servicio produccion
+def stopServer():
+	run('sudo service nginx stop')
+	run('sudo service supervisor stop')
+
+#Arrancar servicio produccion
+def startServer():
+	run('sudo service nginx start')
+	run('sudo service supervisor start')
+
+#Reiniciar servicio produccion
+def restartServer():
+	run('sudo service nginx restart')
+	run('sudo service supervisor restart')
+
+#Actualizar requisitos
+def actRequirements():
+	run('sudo pip install -r DAI/requirements.txt')
+
+#Borrar App
+def removeApp():
+	run('sudo rm -f DAI')
+
+#Clonar App
+def CloneApp():
+	run('sudo git clone https://github.com/javiergarridomellado/DAI.git')
